487681bb371a6550eb521d3cae8c0be23e4295a0
mgsrevolver <clayreimus@gmail.com>
2023-11-30 16:51:03 -0500

    u

--- api.js
+++ api.js
@@ -20,10 +20,6 @@ urls.forEach((url) => {
           let buffer = Buffer.concat(data)
           let feed =
             GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(buffer)
-          let closestTimes = {
-            A43N: Number.MAX_SAFE_INTEGER,
-            G35N: Number.MAX_SAFE_INTEGER,
-          }
           feed.entity.forEach(function (entity, index) {
             if (entity.tripUpdate) {
               entity.tripUpdate.stopTimeUpdate.forEach(function (
@@ -35,25 +31,15 @@ urls.forEach((url) => {
                   (stopTimeUpdate.stopId === 'A43N' ||
                     stopTimeUpdate.stopId === 'G35N')
                 ) {
-                  const arrivalTimeUnix = stopTimeUpdate.arrival.time
-                  if (arrivalTimeUnix < closestTimes[stopTimeUpdate.stopId]) {
-                    closestTimes[stopTimeUpdate.stopId] = arrivalTimeUnix
-                  }
+                  console.log(
+                    `Entity ${index}, arrival time: ${new Date(
+                      stopTimeUpdate.arrival.time * 1000
+                    )}, stopId: ${stopTimeUpdate.stopId}`
+                  )
                 }
               })
             }
           })
-
-          const currentTimeUnix = Math.floor(Date.now() / 1000)
-          const nextArrivalA43N = Math.round(
-            (closestTimes['A43N'] - currentTimeUnix) / 60
-          )
-          const nextArrivalG35N = Math.round(
-            (closestTimes['G35N'] - currentTimeUnix) / 60
-          )
-
-          console.log(`Next train at A43N in: ${nextArrivalA43N} minutes`)
-          console.log(`Next train at G35N in: ${nextArrivalG35N} minutes`)
         })
       }
     )
